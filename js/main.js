c3e9df1d9498bbe13c3a875fe70dd98ec6d63b76
thalia <baci0008@algonquinlive.com>
2014-10-02 10:20:36 -0400

    Adding js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-// JavaScript Document
+var sales__title = $('.sales__title');
+var sales__details = $('.sales__details');
+
+var buttonClickHandler = function () {
+	var currentState = sales__details.attr('data-state')
+	if (currentState== 'active') {
+		sales__details.attr('data-state', 'inactive');
+	} else {
+		sales__details.attr('data-state', 'active');
+	}
+};
+
+sales__title.on('click', buttonClickHandler);
